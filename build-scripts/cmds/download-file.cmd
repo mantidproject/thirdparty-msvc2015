4b68ad6d0303d214193bca9af61d2bb4d343e214
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2015-09-30 14:07:59 +0100

    Fix download-file command

--- build-scripts/cmds/download-file.cmd
+++ build-scripts/cmds/download-file.cmd
@@ -1,12 +1,16 @@
+@setlocal
 :: Download a file using cURL
 :: @param %1 Output name
 :: @param %2 URL of resource
-:: @param %* Pass all other options to curl 
+:: @param %3 Optional extra arguments
 if not exist %1 (
+  set destination=%1
+  set url=%2
   echo Downloading file %1
   rem cURL manual suggests some bad cgi implementations
   rem fail if the user agent is not Mozilla/4.0
-  curl %* --progress-bar -L --output %1
+  curl %2 %3 --progress-bar -L --output %1
   ) else (
   echo Skipping download, %1 already exists
 )
+@endlocal
