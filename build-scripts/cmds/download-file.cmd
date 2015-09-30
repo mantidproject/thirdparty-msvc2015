fa5198b59a6bbb96b2752305a5ca44eec0eea6a0
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2015-09-30 13:31:36 +0100

    Allow extra options to curl

--- build-scripts/cmds/download-file.cmd
+++ build-scripts/cmds/download-file.cmd
@@ -1,11 +1,12 @@
 :: Download a file using cURL
 :: @param %1 Output name
 :: @param %2 URL of resource
+:: @param %* Pass all other options to curl 
 if not exist %1 (
   echo Downloading file %1
   rem cURL manual suggests some bad cgi implementations
   rem fail if the user agent is not Mozilla/4.0
-  curl %2 --user-agent "Mozilla/4.0" --progress-bar -L --output %1
+  curl %* --progress-bar -L --output %1
   ) else (
   echo Skipping download, %1 already exists
 )
