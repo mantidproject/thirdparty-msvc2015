a5e84e602d49ef04b73e2fe5ad7ee83bef2f4962
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2015-10-12 13:36:40 +0100

    Switch off EXTERNALLY_CONFIGURED option

--- build-scripts/extras/zlib/zlib.cmake
+++ build-scripts/extras/zlib/zlib.cmake
@@ -5,6 +5,6 @@
 
 # General Flags
 set (BUILD_SHARED_LIBS ON CACHE BOOL "" FORCE)
-set (ZLIB_EXTERNALLY_CONFIGURED ON CACHE BOOL "" FORCE)
+set (ZLIB_EXTERNALLY_CONFIGURED OFF CACHE BOOL "" FORCE)
 # The hdf5 distribution doesn't have the assembler code
 set (AMD64 OFF CACHE BOOL "" FORCE)
