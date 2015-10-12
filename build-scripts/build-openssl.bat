55f334ae8360587cd94ea0b350ad418ce7ab44ac
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2015-10-12 12:03:07 +0100

    Simplify OpenSSL build script with new commands

--- build-scripts/build-openssl.bat
+++ build-scripts/build-openssl.bat
@@ -16,17 +16,9 @@
 @set SRC_PKG=openssl-1.0.2d.tar.gz
 @set BUILD_DIR=%BUILD_ROOT%\openssl
 
-@call try-mkdir.cmd %BUILD_DIR%
-@cd %BUILD_DIR%
-@call download-file.cmd %SRC_PKG% %SRC_PKG_URL%
+@call download-and-extract.cmd %BUILD_DIR%\%SRC_PKG% %SRC_PKG_URL%
 
-:: Requires 2 steps to extract with 7zip
-@set BUILD_ROOT=%SRC_PKG:.tar.gz=%
-if not exist %BUILD_ROOT% (
-  7z x %SRC_PKG% -y
-  7z x %SRC_PKG:.gz=% -y
-)
-@cd %BUILD_ROOT%
+@cd %BUILD_DIR%\openssl-1.0.2d
 :: release build
 @ perl Configure VC-WIN64A --prefix=%INSTALL_ROOT%
 @ call ms\do_win64a
