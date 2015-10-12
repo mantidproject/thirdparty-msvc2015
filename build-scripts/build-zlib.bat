2741398a97703aa2a5b76d0bbdb1eda743204c56
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2015-10-12 11:51:01 +0100

    Simplify zlib build script

--- build-scripts/build-zlib.bat
+++ build-scripts/build-zlib.bat
@@ -14,41 +14,17 @@
 :: patched to build with CMake
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 @set SRC_PKG_URL="http://www.hdfgroup.org/ftp/HDF5/current/src/CMake/CMake-files/ZLib.tar.gz"
-@set SRC_PKG=ZLib.tar.gz
 @set BUILD_DIR=%BUILD_ROOT%\zlib
-
-@call try-mkdir.cmd %BUILD_DIR%
-@cd %BUILD_DIR%
-@call download-file.cmd %SRC_PKG% %SRC_PKG_URL%
-
-@set ZLIB_ROOT=%BUILD_DIR%\ZLib
-@if not exist %ZLIB_ROOT% @call extract-tarball.cmd %SRC_PKG% %CD%
-cd %ZLIB_ROOT%
+@set SRC_PKG=ZLib.tar.gz
+@call download-and-extract.cmd %BUILD_DIR%\%SRC_PKG% %SRC_PKG_URL%
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Build
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-@if not exist %ZLIB_ROOT%\build mkdir %ZLIB_ROOT%\build
-cd %ZLIB_ROOT%\build
-cmake -G"Visual Studio 14 2015 Win64" -C %ZLIB_EXTRAS_DIR%\zlib.cmake -DCMAKE_INSTALL_PREFIX=%INSTALL_ROOT% ..
-@call:build-project zlib.vcxproj
-
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: Install
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-@call:build-project INSTALL.vcxproj
+@call cmake-build-and-install %BUILD_DIR%\ZLib\ %ZLIB_EXTRAS_DIR%\zlib.cmake %INSTALL_ROOT% zlib.vcxproj
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Finalize
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 @call try-pause.cmd
-goto:eof
-
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: Functions
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: %1 Project file path
-:build-project
-msbuild /nologo /p:Configuration=Release %1
-msbuild /nologo /p:Configuration=Debug %1
 goto:eof
