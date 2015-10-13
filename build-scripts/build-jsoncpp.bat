7a5cbfa7c7c69662a048c094e4eddf6fac8a1249
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2015-10-13 12:44:15 +0100

    Remove old copy-and-pasted code

--- build-scripts/build-jsoncpp.bat
+++ build-scripts/build-jsoncpp.bat
@@ -27,21 +27,6 @@
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 @call cmake-build-and-install %SRC_ROOT%\ %JSONCPP_EXTRAS_DIR%\jsoncpp.cmake %INSTALL_ROOT% src\lib_json\jsoncpp_lib.vcxproj
 
-:: TKBO,TKBRep,TKernel,TKG2d,TKG3d,TKGeomAlgo,TKGeomBase,TKMath,TKPrim
-::,TKTopAlgo
-::@call cmake-configure %SRC_ROOT%\ -C "%OCE_EXTRAS_DIR%\oce.cmake" ^
-:: "-DCMAKE_INSTALL_PREFIX=%INSTALL_ROOT%" "-DOCE_INSTALL_PREFIX=%INSTALL_ROOT%"
-::cd %SRC_ROOT%\build\adm\cmake
-
-:: Requires 2 calls to build all as the script can only take 8 project arguments
-::set PROJECTS=TKBO\TKBO.vcxproj TKBRep\TKBRep.vcxproj TKernel\TKernel.vcxproj ^
-:: TKG2d\TKG2d.vcxproj TKG3d\TKG3d.vcxproj
-::@call build-and-install %PROJECTS%
-::set PROJECTS=TKGeomAlgo\TKGeomAlgo.vcxproj TKGeomBase\TKGeomBase.vcxproj ^
-:: TKMath\TKMath.vcxproj TKMesh\TKMesh.vcxproj TKPrim\TKPrim.vcxproj TKTopAlgo\TKTopAlgo.vcxproj
-::@call build-and-install %PROJECTS%
-
-
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Finalize
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
