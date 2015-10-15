da5bb268b719cb71465f5f579c25098927c529ba
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2015-10-15 12:42:26 +0100

    Always build for x64 platform

--- build-scripts/cmds/build-release-and-debug.cmd
+++ build-scripts/cmds/build-release-and-debug.cmd
@@ -1,5 +1,5 @@
 @setlocal
-:: High-level command to build a set VS projects in both debug and release
+:: High-level command to build a set VS projects in both debug and release (64-bit)
 :: %1-%9 Paths to projects to build
 set project_files=%1 %2 %3 %4 %5 %6 %7 %8 %9
 for %%P in (%project_files%) do (
@@ -13,6 +13,6 @@ goto:eof
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: %1 Project file path
 :do-build-release-and-debug
-msbuild /nologo /p:Configuration=Release %1
-msbuild /nologo /p:Configuration=Debug %1
+msbuild /nologo /p:Configuration=Release /p:Platform=x64 %1
+msbuild /nologo /p:Configuration=Debug /p:Platform=x64 %1
 goto:eof
