283a112cc72f7d09c6fa4487770c38e5e7969823
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2015-11-04 14:33:47 +0000

    Change sip/pyqt builds to be able to link debug builds to python27.lib
    PyQt4 is built in both release and debug and installed in separate
    locations allowing the debug builds to find the correct libraries.

--- build-scripts/build-sip-pyqt4.bat
+++ build-scripts/build-sip-pyqt4.bat
@@ -10,22 +10,25 @@
 @set SIP_EXTRAS_DIR=%~dp0extras\sip-pyqt4
 @set INCLUDE=%INSTALL_ROOT%\include;%INCLUDE%
 @set LIB=%INSTALL_ROOT%\lib;%LIB%
-@set PATH_NO_QT=%INSTALL_ROOT%\bin;%INSTALL_ROOT%\lib\python2.7;D:\Builds\jom;%PATH%
-@set PATH=%PATH_NO_QT%
+@set QT_ROOT=%INSTALL_ROOT%\lib\qt4
+@set PYTHONHOME=%INSTALL_ROOT%\lib\python2.7
+@set PATH_NO_QT=%INSTALL_ROOT%\bin;%PYTHONHOME%;D:\Builds\jom;%PATH%
+@set PATH=%QT_ROOT%\bin;%PATH_NO_QT%
+
+@set BUILD_DIR=%BUILD_ROOT%\sip-pyqt4
+@call try-mkdir %BUILD_DIR%
+@cd %BUILD_DIR%
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: SIP
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-@set SIP_PKG_URL="https://downloads.sourceforge.net/project/pyqt/sip/sip-4.16.9/sip-4.16.9.zip"
-@set BUILD_DIR=%BUILD_ROOT%\sip-pyqt4
-@set SIP_PKG=sip-4.16.9.zip
-@set SIP_ROOT=%BUILD_DIR%\%SIP_PKG:~0,-4%
-@call download-and-extract.cmd %BUILD_DIR%\%SIP_PKG% %SIP_PKG_URL%
+@set SIP_REMOTE_URL="https://github.com/martyngigg/sip-msvc.git"
+@set SIP_ROOT=%BUILD_DIR%\sip-msvc
+@call:git-fetch %SIP_REMOTE_URL% %SIP_ROOT%
 
-:: Standard Qt build
-@set PATH=%INSTALL_ROOT%\lib\qt4\bin;%PATH_NO_QT%
-echo %PATH%
 cd %SIP_ROOT%
+@echo Copying wrappython.h to Python include
+copy /Y %SIP_ROOT%\contrib\wrappython.h %PYTHON_INSTALL_ROOT%\Include\wrappython.h
 :: copy the required mkspec to the required folder
 @copy /Y %INSTALL_ROOT%\lib\qt4\mkspecs\%QMAKESPEC%\qmake.conf specs\%QMAKESPEC%
 @call python configure.py --platform=%QMAKESPEC%
@@ -33,20 +36,25 @@ cd %SIP_ROOT%
 @call nmake install
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: PyQt4 download
+:: PyQt4 (release/build). Use two build directoriesas the build is in place
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-@set PYQT4_PKG_URL="https://downloads.sf.net/project/pyqt/PyQt4/PyQt-4.11.4/PyQt-win-gpl-4.11.4.zip"
-@set PYQT4_PKG=PyQt-win-gpl-4.11.4.zip
-@set PYQT4_ROOT=%BUILD_DIR%\%PYQT4_PKG:~0,-4%
-@call download-and-extract.cmd %BUILD_DIR%\%PYQT4_PKG% %PYQT4_PKG_URL%
+@set QMAKESPEC=%QT_ROOT%\mkspecs\win32-msvc2015
+@set PYQT_REMOTE_URL="https://github.com/martyngigg/pyqt-msvc.git"
+@set PYQT_ROOT=%BUILD_DIR%\PyQt4
+if not exist %PYQT_ROOT% mkdir %PYQT_ROOT%
+cd %PYQT_ROOT%
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-:: release build with static Qt
+:: release build
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-@set PATH=%BUILD_ROOT%\Qt-static\qt-everywhere-opensource-src-4.8.6\bin;%PATH_NO_QT%
-cd %PYQT4_ROOT%
-:: Disable phonon as it won't compile
-if not exist configure.py.orig call patch -p0 --input=%SIP_EXTRAS_DIR%\pyqt4\configure.py.patch --backup
+@set PYQT_ROOT_RELEASE=%PYQT_ROOT%\release-build
+@call:git-fetch %PYQT_REMOTE_URL% %PYQT_ROOT_RELEASE%
+cd %PYQT_ROOT_RELEASE%
+:: Generate qt.conf next to the qtdirs.exe so it knows where to find Qt
+if not exist release mkdir release
+@set QT_CONF_FILE=release\qt.conf
+echo [Paths] > %QT_CONF_FILE%
+echo Prefix = %QT_ROOT:\=/% >> %QT_CONF_FILE%
 
 :: There is an issue compiling pylupdate. Moc hangs on translator.h in the makefile so we compile it first
 pushd pylupdate
@@ -54,9 +62,31 @@ del moc_translator.cpp 2> nul
 del moc_translator.obj 2> nul
 @call moc.exe -o moc_translator.cpp translator.h
 popd
-@call python configure.py --confirm-license --qsci-api -w -g --no-designer-plugin
-:: jom doesn't seem to work correctly
-@call nmake
+@call python configure.py --confirm-license --qsci-api --no-designer-plugin
+@call jom -j8
+@call nmake install
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: debug build
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+@set PYQT_ROOT_DEBUG=%BUILD_DIR%\PyQt4\debug-build
+@call:git-fetch %PYQT_REMOTE_URL% %PYQT_ROOT_DEBUG%
+cd %PYQT_ROOT_DEBUG%
+:: Generate qt.conf next to the qtdirs.exe so it knows where to find Qt
+if not exist debug mkdir debug
+@set QT_CONF_FILE=debug\qt.conf
+echo [Paths] > %QT_CONF_FILE%
+echo Prefix = %QT_ROOT:\=/% >> %QT_CONF_FILE%
+
+:: There is an issue compiling pylupdate. Moc hangs on translator.h in the makefile so we compile it first
+pushd pylupdate
+del moc_translator.cpp 2> nul
+del moc_translator.obj 2> nul
+@call moc.exe -o moc_translator.cpp translator.h
+popd
+:: -u: Build with debug symbols
+@call python configure.py --confirm-license --qsci-api --no-designer-plugin -u --destdir=%PYTHONHOME%\msvc-site-packages\debug
+@call jom -j8
 @call nmake install
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -73,4 +103,16 @@ move /Y %PYQTCONFIG%.fixed %PYQTCONFIG%
 :: Finalize
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 @call try-pause.cmd
+goto:eof
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Functions
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: %1 Remote URL
+:: %2 Destination
+:git-fetch
+if not exist %2 (
+  echo Cloning from %1
+  call git clone %1 %2
+)
 goto:eof
