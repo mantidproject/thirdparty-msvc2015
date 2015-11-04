72f3b04343196263905fa67e10770e8438b59b67
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2015-11-04 14:36:23 +0000

    Add patched sip that can build debug libs linked to python27.lib
    Also adds PyQt4 built in release and debug

--- lib/python2.7/pyuic4.bat
+++ lib/python2.7/pyuic4.bat
@@ -1,1 +1,1 @@
-@"D:\GitHub\thirdparty-msvc2015\lib\python2.7\python.exe" "D:\GitHub\thirdparty-msvc2015\lib\python2.7\Lib\site-packages\PyQt4\uic\pyuic.py" %1 %2 %3 %4 %5 %6 %7 %8 %9
+@"d:\GitHub\mantid\external\thirdparty-msvc2015\lib\python2.7\python.exe" "d:\GitHub\mantid\external\thirdparty-msvc2015\lib\python2.7\msvc-site-packages\debug\PyQt4\uic\pyuic.py" %1 %2 %3 %4 %5 %6 %7 %8 %9
