d0aa98b561a287e9f98000bef0208351dac46c93
Martyn Gigg <martyn.gigg@gmail.com>
2016-07-14 15:54:52 +0100

    Fix absolute path in pyuic4

--- lib/python2.7/pyuic4.bat
+++ lib/python2.7/pyuic4.bat
@@ -1,1 +1,2 @@
-@"d:\GitHub\mantid\external\thirdparty-msvc2015\lib\python2.7\python.exe" "d:\GitHub\mantid\external\thirdparty-msvc2015\lib\python2.7\msvc-site-packages\debug\PyQt4\uic\pyuic.py" %1 %2 %3 %4 %5 %6 %7 %8 %9
+@set _this_dir=%~dp0
+@"%this_dir%\python.exe" "%_this_dir%\Lib\site-packages\PyQt4\uic\pyuic.py" %1 %2 %3 %4 %5 %6 %7 %8 %9
