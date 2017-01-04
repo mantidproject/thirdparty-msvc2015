1bfc9a1c04ae924236c2d30351f7324c6934f013
Moore <lamar.moore@stfc.ac.uk>
2017-01-04 13:52:17 +0000

    patches to libkafka vs projects #14

--- build-scripts/build-librdkafka.bat
+++ build-scripts/build-librdkafka.bat
@@ -8,7 +8,7 @@
 :: Setup environment. Important to ensure correct detection of environment
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 @call %~dp0cmds\common-setup.cmd
-
+@set LIBRDKAFKA_EXTRAS_DIR=%~dp0extras\librdkafka
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Download and unpack source
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -20,7 +20,7 @@
 @cd %BUILD_DIR%
 @call download-file.cmd %SRC_PKG% %SRC_PKG_URL%
 
-set LIBRDKAFKA_ROOT=%BUILD_DIR%\librdkafka-0.9.2
+@set LIBRDKAFKA_ROOT=%BUILD_DIR%\librdkafka-0.9.2
 @call extract-zip-file.cmd %SRC_PKG% %CD%
 
 
@@ -30,15 +30,12 @@ set LIBRDKAFKA_ROOT=%BUILD_DIR%\librdkafka-0.9.2
 @set VS_BUILD_DIR=%LIBRDKAFKA_ROOT%\win32
 @cd %VS_BUILD_DIR%
 
-:: Download nuget for missing packages
-@set NUGET_PKG_URL="https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
-@set NUGET_PKG=nuget.exe
-@call download-file.cmd %NUGET_PKG% %NUGET_PKG_URL%
-
-::Assume OpenSSL has already been downloaded
-@set PATH=%PATH%;%INSTALL_ROOT%\include;%INSTALL_ROOT%\bin;
-::install missing packages
-nuget install packages.config -outputdirector %VS_BUILD_DIR%\packages
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Patch
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+@set MANTIDTHIRDPARTY=%INSTALL_ROOT%
+@call patch -p0 --input=%LIBRDKAFKA_EXTRAS_DIR%\librdkafka.vcxproj.patch --backup
+@call patch -p0 --input=%LIBRDKAFKA_EXTRAS_DIR%\librdkafkacpp.vcxproj.patch --backup
 
 @call:build-rdkafka Release
 @call:build-rdkafka Debug
@@ -50,8 +47,8 @@ if not exist %INSTALL_ROOT%\include\librdkafka mkdir %INSTALL_ROOT%\include\libr
 xcopy %LIBRDKAFKA_ROOT%\src-cpp\*.h %INSTALL_ROOT%\include\librdkafka /Y
 xcopy %LIBRDKAFKA_ROOT%\src\*.h %INSTALL_ROOT%\include\librdkafka /Y
 
-@call:install-libs-Release
-@call:install-libs-Debug
+@call:install-libs Release
+@call:install-libs Debug
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Functions
@@ -61,20 +58,9 @@ xcopy %LIBRDKAFKA_ROOT%\src\*.h %INSTALL_ROOT%\include\librdkafka /Y
 msbuild /nologo /p:Configuration=%1 /p:Platform=x64 /p:PlatformToolset=v140 %VS_BUILD_DIR%\librdkafka.sln
 goto:eof
 
-:install-libs-Release
-xcopy %VS_BUILD_DIR%\x64\Release\librdkafkacpp.lib %INSTALL_ROOT%\lib /Y /I
-xcopy %VS_BUILD_DIR%\x64\Release\librdkafkacpp.pdb %INSTALL_ROOT%\bin /Y /I
-xcopy %VS_BUILD_DIR%\x64\Release\librdkafkacpp.dll %INSTALL_ROOT%\bin /Y /I
-xcopy %VS_BUILD_DIR%\x64\Release\librdkafka.lib %INSTALL_ROOT%\lib /Y /I
-xcopy %VS_BUILD_DIR%\x64\Release\librdkafka.pdb %INSTALL_ROOT%\bin /Y /I
-xcopy %VS_BUILD_DIR%\x64\Release\librdkafka.dll %INSTALL_ROOT%\bin /Y /I
-goto:eof
-
-:install-libs-Debug
-copy %VS_BUILD_DIR%\x64\Debug\librdkafkacpp.lib %INSTALL_ROOT%\lib\librdkafkacppd.lib /Y 
-copy %VS_BUILD_DIR%\x64\Debug\librdkafkacpp.pdb %INSTALL_ROOT%\bin\librdkafkacppd.pdb /Y 
-copy %VS_BUILD_DIR%\x64\Debug\librdkafkacpp.dll %INSTALL_ROOT%\bin\librdkafkacppd.dll /Y 
-copy %VS_BUILD_DIR%\x64\Debug\librdkafka.lib %INSTALL_ROOT%\lib\librdkafkad.lib /Y 
-copy %VS_BUILD_DIR%\x64\Debug\librdkafka.pdb %INSTALL_ROOT%\bin\librdkafkad.pdb /Y 
-copy %VS_BUILD_DIR%\x64\Debug\librdkafka.dll %INSTALL_ROOT%\bin\librdkafkad.dll /Y 
+:: %1 Configuration to build: Release/Debug
+:install-libs
+xcopy %VS_BUILD_DIR%\x64\%1\librdkafka*.lib %INSTALL_ROOT%\lib /Y /I
+xcopy %VS_BUILD_DIR%\x64\%1\librdkafka*.pdb %INSTALL_ROOT%\bin /Y /I
+xcopy %VS_BUILD_DIR%\x64\%1\librdkafka*.dll %INSTALL_ROOT%\bin /Y /I
 goto:eof
