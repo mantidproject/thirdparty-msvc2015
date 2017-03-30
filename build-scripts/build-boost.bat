9a4fb756ecd92ac113b9d5dbe3c08f1575cfbe9a
LamarMoore <lamar.moore@stfc.ac.uk>
2017-03-30 09:17:17 +0100

    Updated boost to 1.63 and added mpi libs re #17

--- build-scripts/build-boost.bat
+++ build-scripts/build-boost.bat
@@ -18,15 +18,44 @@
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Download and unpack source
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-@set SRC_PKG_URL="http://downloads.sourceforge.net/project/boost/boost/1.59.0/boost_1_59_0.zip?r=http%3A%2F%2Fwww.boost.org%2Fusers%2Fhistory%2Fversion_1_59_0.html&ts=1443101740&use_mirror=kent"
-@set SRC_PKG=boost_1_59_0.zip
+::Fetch Microsoft MPI
+@set MS_MPI_SDK_PACKAGE_URL="https://download.microsoft.com/download/B/2/E/B2EB83FE-98C2-4156-834A-E1711E6884FB/msmpisdk.msi"
+@set MS_MPI_SDK_PACKAGE=msmpisdk.msi
+@set MS_MPI_PACKAGE_URL="https://download.microsoft.com/download/B/2/E/B2EB83FE-98C2-4156-834A-E1711E6884FB/MSMpiSetup.exe"
+@set MS_MPI_PACKAGE=MSMpiSetup.exe
+
+::Fetch Boost 1.63
+@set SRC_PKG_URL="https://netix.dl.sourceforge.net/project/boost/boost/1.63.0/boost_1_63_0.zip"
+@set SRC_PKG=boost_1_63_0.zip
 @set BUILD_DIR=%BUILD_ROOT%\boost
+
+
 
 @call try-mkdir.cmd %BUILD_DIR%
 @cd %BUILD_DIR%
+
+::install MS MPI if it doesnt exit
+if not exist "C:/Program Files/Microsoft MPI" (
+    call download-file.cmd %MS_MPI_PACKAGE% %MS_MPI_PACKAGE_URL%
+    call %BUILD_DIR%\MSMpiSetup.exe
+)
+
+::install MS MPI SDK if it doesnt exist
+if not exist "C:/Program Files (x86)/Microsoft SDKs/MPI" (
+    call download-file.cmd %MS_MPI_SDK_PACKAGE% %MS_MPI_SDK_PACKAGE_URL%
+    call msiexec.exe /i %BUILD_DIR%\msmpisdk.msi
+)
+
+::move MS MPI into repo
+xcopy "C:\Program Files (x86)\Microsoft SDKs\MPI\Include\*.h" %INSTALL_ROOT%\include /Y /I
+if not exist %INSTALL_ROOT%\include\x64 call try-mkdir.cmd %INSTALL_ROOT%\include\x64
+xcopy "C:\Program Files (x86)\Microsoft SDKs\MPI\Include\x64\*.h" %INSTALL_ROOT%\include /Y /I
+xcopy "C:\Program Files (x86)\Microsoft SDKs\MPI\Lib\x64\*.lib" %INSTALL_ROOT%\lib /Y /I
+xcopy "C:\Program Files (x86)\Microsoft SDKs\MPI\Lib\x64\*.lib" %INSTALL_ROOT%\lib /Y /I
+
 @call download-file.cmd %SRC_PKG% %SRC_PKG_URL%
 
-set BOOST_ROOT=%BUILD_DIR%\boost_1_59_0
+set BOOST_ROOT=%BUILD_DIR%\boost_1_63_0
 @call extract-zip-file.cmd %SRC_PKG% %CD%
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
@@ -34,6 +63,9 @@ set BOOST_ROOT=%BUILD_DIR%\boost_1_59_0
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 cd %BOOST_ROOT%
 if not exist include\boost\type_traits\intrinsics.hpp.orig call patch -p0 --input=%BOOST_EXTRAS_DIR%\type-traits-160.patch --backup
+
+:: use custom project configuration which includes mpi build
+xcopy %INSTALL_ROOT%\build-scripts\extras\boost\project-config.jam %BOOST_ROOT% /Y /I
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Build & Install
@@ -52,7 +84,7 @@ if exist "%INSTALL_ROOT%\include\boost" (
 :: Build libraries with no external dependencies
 :: --layout=tagged ensures the the include directory is %prefix%/include/boost and not %prefix%/include/boost_X_XX_X/boost
 set COMMON_BUILD_OPTS=link=shared threading=multi address-model=64 runtime-link=shared
-@call b2.exe %COMMON_BUILD_OPTS% --with-date_time --with-regex --with-python variant=release variant=debug install --prefix=%INSTALL_ROOT% --layout=tagged
+@call b2.exe %COMMON_BUILD_OPTS% --with-date_time --with-regex --with-python --with-serialization --with-mpi variant=release variant=debug install --prefix=%INSTALL_ROOT% --layout=tagged
 
 :: We want the dlls in bin but b2 won't allow that
 echo Moving dlls to %INSTALL_ROOT%\bin
