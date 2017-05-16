2ebda2efc054757d4fd63e598b2dc22c87b353ac
Martyn Gigg <martyn.gigg@gmail.com>
2017-05-16 13:10:18 +0100

    Fix invalid variable name in wrapper script

--- lib/python2.7/pyuic4.bat
+++ lib/python2.7/pyuic4.bat
@@ -1,2 +1,2 @@
 @set _this_dir=%~dp0
-@"%this_dir%\python.exe" "%_this_dir%\Lib\site-packages\PyQt4\uic\pyuic.py" %1 %2 %3 %4 %5 %6 %7 %8 %9
+@"%_this_dir%\python.exe" "%_this_dir%\Lib\site-packages\PyQt4\uic\pyuic.py" %1 %2 %3 %4 %5 %6 %7 %8 %9
