4544e3bfb9551aa75fca521417a34bcd3ece5f03
Lamar Moore <lamar.moore@stfc.ac.uk>
2018-07-20 07:22:10 +0100

    sip updated to 4.19.12

--- build-scripts/build-sip.bat
+++ build-scripts/build-sip.bat
@@ -26,14 +26,24 @@ if exist %PYTHONHOME% (
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: SIP
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-@set SRC_PKG_URL="https://downloads.sourceforge.net/project/pyqt/sip/sip-4.19.3/sip-4.19.3.zip"
-@set SRC_PKG=sip-4.19.3.zip
+@set SRC_PKG_URL="https://sourceforge.net/projects/pyqt/files/sip/sip-4.19.12/sip-4.19.12.zip"
+@set SRC_PKG=sip-4.19.12.zip
 call download-file.cmd %SRC_PKG% %SRC_PKG_URL%
-@set SIP_ROOT=%BUILD_DIR%\sip-4.19.3
+@set SIP_ROOT=%BUILD_DIR%\sip-4.19.12
 if not exist %SIP_ROOT% call extract-zip-file.cmd %SRC_PKG% %CD%
 cd /d %SIP_ROOT%
 :: Patch
-if not exist siplib\sip.h.in.orig call patch -p0 --input=%SIP_EXTRAS_DIR%\sip-wrapython.patch --backup
+if not exist siplib\apiversions.c.orig call patch -p0 --input=%SIP_EXTRAS_DIR%\apiversions.c.patch --backup
+if not exist siplib\array.c.orig call patch -p0 --input=%SIP_EXTRAS_DIR%\array.c.patch --backup
+if not exist siplib\array.h.orig call patch -p0 --input=%SIP_EXTRAS_DIR%\array.h.patch --backup
+if not exist siplib\descriptors.c.orig call patch -p0 --input=%SIP_EXTRAS_DIR%\descriptors.c.patch --backup
+if not exist siplib\int_convertors.c.orig call patch -p0 --input=%SIP_EXTRAS_DIR%\int_convertors.c.patch --backup
+if not exist siplib\qtlib.c.orig call patch -p0 --input=%SIP_EXTRAS_DIR%\qtlib.c.patch --backup
+if not exist siplib\sipint.h.orig call patch -p0 --input=%SIP_EXTRAS_DIR%\sipint.h.patch --backup
+if not exist siplib\siplib.c.orig call patch -p0 --input=%SIP_EXTRAS_DIR%\siplib.c.patch --backup
+if not exist siplib\sip.h.orig call patch -p0 --input=%SIP_EXTRAS_DIR%\sip.h.patch --backup
+if not exist siplib\voidptr.c.orig call patch -p0 --input=%SIP_EXTRAS_DIR%\voidptr.c.patch --backup
+if not exist sipgen\gencode.c.orig call patch -p0 --input=%SIP_EXTRAS_DIR%\gencode.c.patch --backup
 if not exist siputils.py.orig call patch -p0 --input=%SIP_EXTRAS_DIR%\siputils-no-debug.patch --backup
 call python configure.py --platform=%QMAKESPEC%
 call nmake
