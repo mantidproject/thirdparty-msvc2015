c9563d956f63c54c1bf4af76941fa18bccc3e640
Lamar Moore <lamar.moore@stfc.ac.uk>
2018-11-29 12:22:11 +0000

    Updated nexus library

--- build-scripts/build-nexus.bat
+++ build-scripts/build-nexus.bat
@@ -19,28 +19,31 @@
 @set LOCAL_INSTALL_UNIX=%LOCAL_INSTALL_PREFIX:\=/%
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: HDF
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+@echo Downloading HDF Dependencies
+@set CMAKEHDF_PKG_URL="https://support.hdfgroup.org/ftp/HDF/HDF_Current/src/CMake-hdf-4.2.13.tar.gz"
+@set CMAKEHDF_PKG=CMake-hdf-4.2.13.tar.gz
+@set CMAKEHDF_SRC_ROOT=%BUILD_DIR%\CMake-hdf-4.2.13
+if not exist %CMAKEHDF_SRC_ROOT% call download-and-extract.cmd %BUILD_DIR%\%CMAKEHDF_PKG% %CMAKEHDF_PKG_URL%
+
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: JPEG
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 @echo Building JPEG dependency
-@set JPEG_PKG_URL="http://www.hdfgroup.org/ftp/HDF/HDF_Current/src/CMake/CMake-files/JPEG8b.tar.gz"
-@set JPEG_PKG=JPEG8b.tar.gz
-@set JPEG_SRC_ROOT=%BUILD_DIR%\JPEG8b
-if not exist %JPEG_SRC_ROOT% call download-and-extract.cmd %BUILD_DIR%\%JPEG_PKG% %JPEG_PKG_URL%
+@set JPEG_PKG_URL=%CMAKEHDF_SRC_ROOT%\JPEG8d.tar.gz
+@set JPEG_PKG=%JPEG_PKG_URL%
+@set JPEG_SRC_ROOT=%CMAKEHDF_SRC_ROOT%\JPEG8d
+if not exist %JPEG_SRC_ROOT% call download-and-extract.cmd %JPEG_PKG% %JPEG_PKG_URL%
 
 cd %JPEG_SRC_ROOT%
-if not exist config\cmake\JPEG-config.cmake.install.in.orig patch -p0 --input=%NEXUS_EXTRAS_DIR%\JPEG-config.cmake.install.in.patch --backup
 @call cmake-configure.cmd %JPEG_SRC_ROOT%\ "-C%NEXUS_EXTRAS_DIR%\jpeg.cmake" "-DCMAKE_INSTALL_PREFIX=%LOCAL_INSTALL_PREFIX%"
 @call build-and-install.cmd %JPEG_SRC_ROOT%\build ALL_BUILD.vcxproj
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: HDF4
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-@echo Building HDF4 dependency
-@set HDF4_PKG_URL="http://www.hdfgroup.org/ftp/HDF/HDF_Current/src/hdf-4.2.11.zip"
-@set HDF4_PKG=hdf-4.2.11.zip
-@set HDF4_SRC_ROOT=%BUILD_DIR%\hdf-4.2.11
-if not exist %HDF4_SRC_ROOT% call download-and-extract.cmd %BUILD_DIR%\%HDF4_PKG% %HDF4_PKG_URL%
-
+@set HDF4_SRC_ROOT=%CMAKEHDF_SRC_ROOT%\hdf-4.2.13
 cd %HDF4_SRC_ROOT%
 if not exist config\cmake_ext_mod\HDFTests.c.orig patch -p0 --input=%NEXUS_EXTRAS_DIR%\HDFTests.c.patch --backup
 if not exist config\cmake_ext_mod\ConfigureChecks.cmake.orig patch -p0 --input=%NEXUS_EXTRAS_DIR%\ConfigureChecks.cmake.patch --backup
@@ -53,18 +56,19 @@ if not exist config\cmake_ext_mod\ConfigureChecks.cmake.orig patch -p0 --input=%
 :: Nexus
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 @echo Building NeXus
-@set NXS_PKG_URL="https://github.com/nexusformat/code/archive/4.3.3.zip"
-@set NXS_PKG=4.3.3.zip
-@set NXS_SRC_ROOT=%BUILD_DIR%\code-4.3.3
+@set NXS_PKG_URL="https://github.com/nexusformat/code/archive/v4.4.3.zip"
+@set NXS_PKG=4.4.3.zip
+@set NXS_SRC_ROOT=%BUILD_DIR%\code-4.4.3
 if not exist %NXS_SRC_ROOT% call download-and-extract.cmd %BUILD_DIR%\%NXS_PKG% %NXS_PKG_URL%
 
 cd %NXS_SRC_ROOT%
+if not exist bindings/cpp/NeXusFile.cpp.orig patch -p0 --input=%NEXUS_EXTRAS_DIR%\NeXusFile.cpp.patch --backup
 if not exist cmake_include\FindHDF4.cmake.orig patch -p0 --input=%NEXUS_EXTRAS_DIR%\FindHDF4.cmake.patch --backup
 
 :: HDF libraries. Scripts require an environment variable
 @set HDF4_ROOT=%LOCAL_INSTALL_UNIX%
 @set HDF5_ROOT=%INSTALL_PREFIX_UNIX%
-@call cmake-configure.cmd %NXS_SRC_ROOT%\ "-C%NEXUS_EXTRAS_DIR%\nexus.cmake" "-DCMAKE_INSTALL_PREFIX=%LOCAL_INSTALL_UNIX%"
+@call cmake-configure.cmd %NXS_SRC_ROOT%\ -C%NEXUS_EXTRAS_DIR%\nexus.cmake "-DCMAKE_INSTALL_PREFIX=%LOCAL_INSTALL_UNIX%" "-DENABLE_CXX=ON" "-DENABLE_HDF4=ON" "-DCMAKE_DEBUG_POSTFIX=D"
 @call build-release-and-debug.cmd %NXS_SRC_ROOT%\build\src\NeXus_Shared_Library.vcxproj %NXS_SRC_ROOT%\build\src\INSTALL.vcxproj
 @call build-release-and-debug.cmd %NXS_SRC_ROOT%\build\bindings\cpp\NeXus_CPP_Shared_Library.vcxproj %NXS_SRC_ROOT%\build\bindings\cpp\INSTALL.vcxproj
 @call build-release-and-debug.cmd %NXS_SRC_ROOT%\build\include\INSTALL.vcxproj
@@ -74,12 +78,16 @@ if not exist cmake_include\FindHDF4.cmake.orig patch -p0 --input=%NEXUS_EXTRAS_D
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 if not exist %INSTALL_PREFIX%\include\nexus mkdir %INSTALL_PREFIX%\include\nexus
 @xcopy %LOCAL_INSTALL_PREFIX%\include\nexus\* %INSTALL_PREFIX%\include\nexus /Y /I
-@xcopy %LOCAL_INSTALL_PREFIX%\include\napi.h %INSTALL_PREFIX%\include /Y /I
-@xcopy %LOCAL_INSTALL_PREFIX%\include\napiu.h %INSTALL_PREFIX%\include /Y /I
-@xcopy %LOCAL_INSTALL_PREFIX%\bin\libNeXus-0*.dll %INSTALL_PREFIX%\bin /Y
-@xcopy %LOCAL_INSTALL_PREFIX%\bin\libNeXusCPP-0*.dll %INSTALL_PREFIX%\bin /Y
-@xcopy %LOCAL_INSTALL_PREFIX%\lib\nexus\libNeXus-0*.lib %INSTALL_PREFIX%\lib /Y
-@xcopy %LOCAL_INSTALL_PREFIX%\lib\nexus\libNeXusCPP-0*.lib %INSTALL_PREFIX%\lib /Y
+@xcopy %LOCAL_INSTALL_PREFIX%\include\nexus\napi.h %INSTALL_PREFIX%\include /Y /I
+@xcopy %LOCAL_INSTALL_PREFIX%\include\nexus\napiu.h %INSTALL_PREFIX%\include /Y /I
+@xcopy %LOCAL_INSTALL_PREFIX%\bin\NeXus.dll %INSTALL_PREFIX%\bin\libNeXus-0.dll /Y
+@xcopy %LOCAL_INSTALL_PREFIX%\bin\NeXusD.dll %INSTALL_PREFIX%\bin\libNeXus-0D.dll /Y
+@xcopy %LOCAL_INSTALL_PREFIX%\bin\NeXusCPP.dll %INSTALL_PREFIX%\bin\libNeXusCPP-0.dll /Y
+@xcopy %LOCAL_INSTALL_PREFIX%\bin\NeXusCPPD.dll %INSTALL_PREFIX%\bin\libNeXusCPP-0D.dll /Y
+@xcopy %LOCAL_INSTALL_PREFIX%\lib\NeXus.lib %INSTALL_PREFIX%\lib\libNeXus-0.lib /Y
+@xcopy %LOCAL_INSTALL_PREFIX%\lib\NeXusD.lib %INSTALL_PREFIX%\lib\libNeXus-0D.lib /Y
+@xcopy %LOCAL_INSTALL_PREFIX%\lib\NeXusCPP.lib %INSTALL_PREFIX%\lib\libNeXusCPP-0.lib /Y
+@xcopy %LOCAL_INSTALL_PREFIX%\lib\NeXusCPPD.lib %INSTALL_PREFIX%\lib\libNeXusCPP-0D.lib /Y
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 :: Install Python bindings
