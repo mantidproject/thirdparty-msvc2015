ea52464d7deeea6b1c5dbe4bb046b156a7acbfe3
Martyn Gigg <martyn.gigg@gmail.com>
2020-01-20 11:37:13 +0000

    Add qt4/lib DLL directory

--- build-scripts/extras/python/sitecustomize.py
+++ build-scripts/extras/python/sitecustomize.py
@@ -1,32 +1,28 @@
 """
-Attempts to detect if this bundle is run from a development build and updates the PATH
-if so to find all depedent libraries. A packaged bundle has all DLLs in the main bin directory
-and requires no further updating. In this case we can safely assume this bundle is only
-ever used on a Windows platform. This trick is based on the similar find_qt functionality
+Attempts to detect if this bundle is run from a development build and updates the DLL search path
+to find all thirdparty libraries. A packaged bundle has all DLLs in the main bin directory
+and requires no action.
+
+This bundle is assumed to only be used on Windows. This trick is based on the similar find_qt functionality
 in PyQt5.__init__.
 """
 
 def find_thirdparty():
     import os, sys
 
-    executable = sys.executable
-    # don't do anything unless we are Python
-    if "python" not in executable:
-        return
-
+    # Look for a "marker" DLL that indicates that this looks like the layout in a user package.
+    py_exe_dir = os.path.normpath(os.path.dirname(os.__file__) + '\\..')
     marker_dll = '\\zlib.dll'
-    # Look for a "marker" dll to signify we are in a development build layout
-    py_exe_dir = os.path.dirname(executable)
     if os.path.isfile(py_exe_dir + marker_dll):
         # this looks like a package bundle where all dependent DLLs are in the same directory.
-        # nothing to do
+        # nothing to do.
         return
-    
+
     # We assume the layout of the thirdparty directory has DLLs in bin, lib\\qt4 and lib\\qt5
     thirdparty_lib = os.path.dirname(py_exe_dir)
     thirdparty_bin = os.path.dirname(thirdparty_lib) + "\\bin"
-    extra_paths = (thirdparty_bin, thirdparty_lib + "\\qt4\\bin",
-                   thirdparty_lib + "\\qt5\\bin")
+    extra_paths = (thirdparty_bin, thirdparty_lib + "\\qt4\\bin", thirdparty_lib + "\\qt4\\lib",
+                   thirdparty_lib + "\\qt5\\bin", thirdparty_lib + "\\qt5\\lib")
     for dll_dir in extra_paths:
         # Add it to the search path for this process if it exists
         if os.path.isdir(dll_dir):
